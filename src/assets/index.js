4607e441fe25c2c1e90718b113b41f3541a3dd67
Ahmed Shahid <a.razashahid19@gmail.com>
2024-11-25 03:25:04 +0500

    Fix Vercel Module Issue

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,3 @@
-import logoPng from "./images/logoPng.png";
-import logoPng2 from "./images/logoPng2.png";
-import logoJpg from "./images/logoJpg.png";
-import logoSvg from "./images/logoSvg.svg";
 import logo from "./images/logo.png";
 
-export { logoPng, logoPng2, logoJpg, logoSvg, logo };
+export { logo };
