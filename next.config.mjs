5f253d94701ec52334c0d2e400957ff9b7993fc9
Ahmed Shahid <a.razashahid19@gmail.com>
2024-11-25 03:27:03 +0500

    Fix Build Issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "images.unsplash.com" },
+      { hostname: "images.pexels.com" },
       { hostname: "lh3.googleusercontent.com" },
-      { hostname: "res.cloudinary.com" }
+      { hostname: "res.cloudinary.com" },
+      { hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
